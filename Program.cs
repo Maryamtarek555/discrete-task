dc8b5ffb0c560080f7fd8124a19a79863ab589f0
Maryamtarek555 <120967738+Maryamtarek555@users.noreply.github.com>
2022-12-18 20:24:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the number");
+
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("prime number from 2 to {0}:", num);
+            for(int i = 2; i<=num ;i++ )
+            {
+            bool  number = true;
+            for(int j = 2;j<i;j++)
+                {
+                    if (i % j == 0)
+                    {
+                        number = false;
+                        break;
+                    }
+                }
+            if(number)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+
+
+
+
+
+
+                       
+
+
+
+            }
+
+
+
+
+
+                    
+        }
+    }
+}
